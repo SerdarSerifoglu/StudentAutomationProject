d46c68f79d270aae58724436113697aa4f26b36e
serdar serif <serdarserifoglu@gmail.com>
2019-11-17 00:47:55 +0300

    Identity startup ayarları ve db tabloları oluşturuldu

---
+++ StudentAutomationProject/Identity/SapIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAutomationProject.Identity
+{
+    public class SapIdentityDbContext : IdentityDbContext<SapIdentityUser, SapIdentityRole, string>
+    {
+        public SapIdentityDbContext(DbContextOptions<SapIdentityDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- StudentAutomationProject/Startup.cs
+++ StudentAutomationProject/Startup.cs
@@ -6,9 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StudentAutomationProject.Identity;
 
 namespace StudentAutomationProject
 {
@@ -31,6 +34,46 @@ namespace StudentAutomationProject
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<SapIdentityDbContext>(options => options.UseSqlServer(Configuration["dbConnection"]));
+
+            services.AddIdentity<SapIdentityUser, SapIdentityRole>()
+                .AddEntityFrameworkStores<SapIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+
+                //Birçok kez denediðinde kilitlensin
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                //kilitlenen bu kadar dk sonra açýlsýn
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                //yeni kullanýcýlar için geçerli olsun mu
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Security/Login";
+                options.LogoutPath = "/Security/Logout";
+                options.AccessDeniedPath = "/Security/AccessDenied";
+                //Örn. cookie süresi 10 dk ise 6.dk da girildiðinde süreyi yeniler
+                options.SlidingExpiration = true;
+                options.Cookie = new CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name=".StudentAutomation.Cookie",
+                    Path = "/",
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest
+                };
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -51,8 +94,8 @@ namespace StudentAutomationProject
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
+            //app.UseCookiePolicy();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
